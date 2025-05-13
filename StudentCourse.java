0c2470b0ec0610a96170169e77fec9a78dac6ec9
madhubevoor2004 <bevoormadhu@gmsil.com>
2025-05-13 09:58:51 +0530

    save

---
+++ StudentCourse.java
@@ -0,0 +1,43 @@
+package program4;
+
+import java.util.*;
+
+public class StudentCourse {
+    private Map<Integer, List<Course>> courseMap;
+
+    public StudentCourse() {
+        courseMap = new HashMap<>();
+    }
+
+    public void addCourse(int semester, String courseName, int marks) {
+        courseMap.computeIfAbsent(semester, k -> new ArrayList<>())
+                 .add(new Course(courseName, marks));
+    }
+
+    public void displayCourses() {
+        for (int semester : courseMap.keySet()) {
+            System.out.println("Semester " + semester + ":");
+            for (Course c : courseMap.get(semester)) {
+                System.out.println("  " + c.courseName + " - Marks: " + c.marks);
+            }
+        }
+    }
+
+    private static class Course {
+        String courseName;
+        int marks;
+
+        Course(String courseName, int marks) {
+            this.courseName = courseName;
+            this.marks = marks;
+        }
+    }
+
+    public static void main(String[] args) {
+        StudentCourse sc = new StudentCourse();
+        sc.addCourse(1, "Math", 80);
+        sc.addCourse(1, "English", 75);
+        sc.addCourse(2, "Science", 90);
+        sc.displayCourses();
+    }
+}
